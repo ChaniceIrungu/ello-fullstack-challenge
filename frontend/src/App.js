091e8a4cdff84b6ce6ba9a2b908f294776a614c6
Chanice Irungu <chaniceashley@gmail.com>
2024-06-21 02:29:08 +0300

    chr:clean-up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,35 +6,8 @@ import BrowseBooks from './components/BrowseBooks';
 import theme from './theme';
 
 function App() {
-  // const theme = createTheme({
-  //   typography: {
-  //     subtitle1: {
-  //       fontSize: 12,
-  //     },
-  //     body1: {
-  //       fontWeight: 500,
-  //     },
-  //     button: {
-  //       fontStyle: 'italic',
-  //     },
-  //   },
-  // });
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
        <ReadingListProvider>
        <ThemeProvider theme={theme}>
        <CssBaseline />
